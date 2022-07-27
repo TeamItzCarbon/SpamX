d8deb50b1b4a74ca55616f2f1eb77a3bee8d6de3
sᴛᴀʀ ʙᴏɪ <104632716+Starboihacks369@users.noreply.github.com>
2022-07-27 22:40:10 +0530

    Add files via upload

---
+++ StarXSpam/__main__.py
@@ -0,0 +1,349 @@
+# StarXSpam
+
+import asyncio
+import sys
+from sys import argv
+import glob
+from pathlib import Path
+from StarXSpam.utils import load_plugins
+import logging
+from telethon import events
+from . import Star, Star2, Star3, Star4, Star5 , Star6, Star7, Star8, Star9, Star10, Star11, Star12, Star13, Star14, Star15, Star16, Star17, Star18, Star19, Star20, Star21, Star22, Star23, Star24, Star25, Star26, Star27, Star28, Star29, Star30, Star31, Star32, Star33, Star34, Star35, Star36, Star37, Star38, Star39, Star40
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+
+path = "StarXSpam/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("StarX ID Spam Successfully Deployed !!")
+print("Enjoy! Do Visit @Best_FriendsFor_Ever")
+
+if len(argv) not in (1, 3, 4):
+    try:
+        Star.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star2.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star3.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star4.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star5.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star6.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star7.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star8.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star9.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star10.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star11.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star12.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star13.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star14.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star15.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star16.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star17.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star18.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star19.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star20.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star21.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star22.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star23.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star24.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star25.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star26.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star27.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star28.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star29.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star30.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star31.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star32.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star33.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star34.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star35.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star36.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star37.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star38.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star39.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Star40.disconnect()
+    except Exception as e:
+        pass
+else:
+    try:
+        Star.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star2.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star3.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star4.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star5.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star6.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star7.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star8.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star9.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star10.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star11.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star12.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star13.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star14.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star15.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star16.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star17.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star18.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star19.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star20.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star21.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star22.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star23.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star24.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star25.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star26.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star27.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star28.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star29.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star30.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star31.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star32.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star33.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star34.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star35.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star36.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star37.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star38.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star39.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Star40.run_until_disconnected()
+    except Exception as e:
+        pass
